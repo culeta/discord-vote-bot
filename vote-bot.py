16e40063161b686638b2643d694fd42360dce48d
culeta <2163008@stu.hus.ac.jp>
2019-04-27 00:19:59 +0900

    絵文字、リアクションをリスト化・リアクションの追加

--- vote-bot.py
+++ vote-bot.py
@@ -2,6 +2,14 @@ import discord
 
 client = discord.Client()
 
+# リアクションボタン
+reaction_list = ['\U0001F1E6', '\U0001F1E7', '\U0001F1E8', '\U0001F1E9', '\U0001F1EA']
+lst_emoji = ''
+count = 1
+# 投票をした人のリスト
+vote_list = []
+
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -13,29 +21,35 @@ async def on_ready():
 async def on_message(message):
 
     if message.content.startswith('/vote'):
-        tmp = 96
         # メッセージの取得・list化
         msg = message.content[6:]
         msg_list = msg.split()
         # リスト数の取得
+        global count
         count = len(msg.split())
-        # 投票をした人のリスト
-        vote_list = []
+        count -= 1
         # 絵文字のリスト
-        emoji_list = []
-        vote_switch = ':regional_indicator_{}: '
-        test = msg_list[0]
+        emoji_list = [' ', ':regional_indicator_a:', ':regional_indicator_b:', ':regional_indicator_c:',
+                      ':regional_indicator_d:', ':regional_indicator_e:']
+        # 質問内容
+        skip_msg = msg_list[0]
+        # リアクションをつけるメッセージ
+        global lst_emoji
+        lst_emoji = emoji_list[count]
+
         # 送り主がBotだった場合反応したくないので
         if client.user != message.author:
+            await message.channel.send(skip_msg)
 
-            await message.channel.send(msg_list[0])
+            for emoji_list, msg_list in zip(emoji_list, msg_list):
+                if msg_list == skip_msg:
+                    continue
+                await message.channel.send(emoji_list + msg_list)
 
-            for i in range(1, count):
-                tmp += 1
-                revmsg = vote_switch.format(chr(tmp))
-                emoji_list.append(revmsg)
-                await message.channel.send(emoji_list[i - 1] + msg_list[i])
+    if message.content.startswith(lst_emoji):
+        for i in range(0, count):
+            await message.add_reaction(reaction_list[i])
 
 
-# BOTのTokenを張り付ける
+# BotのTokenを張り付ける
 client.run("token")
